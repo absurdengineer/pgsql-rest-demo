22f6cb1e626025145712d04496fd4ecc5939a346
mddalam1 <mddalam1@gmail.com>
2020-12-09 12:44:43 +0530

    Done Basic settings to start server and DB Connectivity.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const express = require('express')
+const morgan = require('morgan')
 const pool = require('./databases/db')
+const student = require('./routes/apis/student.api')
 
+//* Application
+const app = express()
 
+//* Middlewares
+app.use(morgan('dev'))
+
+//* Settings
+const PORT = (process.env.PORT || 3000)
+
+//* Routers
+app.use("/api/students/",student)
+
+//* App Listener
+app.listen(PORT,() => {
+    console.log(`Listening on PORT ${PORT} ...`)
+    console.log(`Server Started http://127.0.0.1:${PORT}/`)
+})
